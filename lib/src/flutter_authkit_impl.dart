258bbe63754d2c3ffc1d2d4f01b3b7a56fe526c1
Netesh5 <netesh.paudel@gmail.com>
2025-03-12 13:09:39 +0545

    Add login, register, and logout methods to FlutterAuthKit with error handling

--- lib/src/flutter_authkit_impl.dart
+++ lib/src/flutter_authkit_impl.dart
@@ -3,9 +3,42 @@ import 'package:flutter_authkit/src/services/dio.dart';
 
 class FlutterAuthKit {
   final DioClient _dioClient = DioClient();
-  Future login({Map<String, dynamic>? params}) async {
+
+  // For Login Purpose
+  Future<T> login<T>(
+      {required String loginEndPont,
+      Map<String, dynamic>? params,
+      required T Function(Map<String, dynamic>) fromJson}) async {
     try {
-      final res = await _dioClient.dio.post('/login', data: params);
+      final res = await _dioClient.dio.post('/$loginEndPont', data: params);
+      return fromJson(res.data);
+    } on AuthErrorHandler catch (e) {
+      throw e.message;
+    }
+  }
+
+  // For Register Purpose
+  Future<T> register<T>(
+      {required String registerEndpoint,
+      Map<String, dynamic>? params,
+      required T Function(Map<String, dynamic>) fromJson}) async {
+    try {
+      final res = await _dioClient.dio.post('/$registerEndpoint', data: params);
+      return fromJson(res.data);
+    } on AuthErrorHandler catch (e) {
+      throw e.message;
+    }
+  }
+
+  // For Logout Purpose
+  Future<T> logout<T>(
+      {required String logoutEndpoint,
+      T Function(Map<String, dynamic>)? fromJson}) async {
+    try {
+      final res = await _dioClient.dio.post('/$logoutEndpoint');
+      if (fromJson != null) {
+        return fromJson(res.data);
+      }
       return res.data;
     } on AuthErrorHandler catch (e) {
       throw e.message;
