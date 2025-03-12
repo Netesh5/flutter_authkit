17cb3d50f9e5b907890ad97c3835091207f38d06
Netesh5 <netesh.paudel@gmail.com>
2025-03-12 14:10:05 +0545

    Refactor DioClient to use TokenService for token management and update request headers accordingly

--- lib/src/services/dio.dart
+++ lib/src/services/dio.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_authkit/src/services/token_service.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class DioClient {
@@ -7,23 +8,33 @@ class DioClient {
 
   Dio get dio => _dio;
 
+  final TokenService _tokenService = TokenService();
+
   void init(
       {required String baseUrl,
-      required String token,
       int connectionTimeoutMs = 5000,
       int receiveTimeoutMs = 3000,
       Map<String, dynamic>? headers}) {
     _dio.options.baseUrl = baseUrl;
     _dio.options.connectTimeout = Duration(milliseconds: connectionTimeoutMs);
     _dio.options.receiveTimeout = Duration(milliseconds: receiveTimeoutMs);
-    _dio.options.headers = headers ??
-        {
-          'Content-Type': 'application/json',
-        };
+    _dio.options.headers = headers ?? {"Accept": "Application/json"};
 
     _dio.interceptors.add(InterceptorsWrapper(
-      onRequest: (options, handler) {
-        options.headers['Authorization'] = 'Bearer $token';
+      onRequest: (options, handler) async {
+        final token = await _tokenService.getToken();
+        if (token != null) {
+          options.headers = {
+            ...(options.headers),
+            "Authorization": "Bearer $token",
+            "Accept": "Application/json",
+          };
+        } else {
+          options.headers = {
+            ...(options.headers),
+            "Accept": "Application/json",
+          };
+        }
         return handler.next(options);
       },
       onResponse: (response, handler) {
