e1d5b5ef906c759c523c3d35f2e279e27639df8a
Netesh5 <netesh.paudel@gmail.com>
2025-03-12 14:26:50 +0545

    Refactor FlutterAuthKit to implement singleton pattern, streamline request handling, and integrate TokenService for token management

--- lib/src/flutter_authkit_impl.dart
+++ lib/src/flutter_authkit_impl.dart
@@ -1,46 +1,72 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_authkit/src/handler/auth_error_handler.dart';
 import 'package:flutter_authkit/src/services/dio.dart';
+import 'package:flutter_authkit/src/services/token_service.dart';
 
 class FlutterAuthKit {
+  static final FlutterAuthKit _instance = FlutterAuthKit._internal();
+  factory FlutterAuthKit() => _instance;
+
   final DioClient _dioClient = DioClient();
+  final TokenService _tokenService = TokenService();
 
-  // For Login Purpose
-  Future<T> login<T>(
-      {required String loginEndPont,
-      Map<String, dynamic>? params,
-      required T Function(Map<String, dynamic>) fromJson}) async {
-    try {
-      final res = await _dioClient.dio.post('/$loginEndPont', data: params);
-      return fromJson(res.data);
-    } on DioException catch (e) {
-      throw AuthErrorHandler.fromDioError(e);
-    }
-  }
+  FlutterAuthKit._internal();
 
-  // For Register Purpose
-  Future<T> register<T>(
-      {required String registerEndpoint,
-      Map<String, dynamic>? params,
-      required T Function(Map<String, dynamic>) fromJson}) async {
+  Future<T> _request<T>({
+    required String endpoint,
+    required String method,
+    Map<String, dynamic>? params,
+    required T Function(Map<String, dynamic>) fromJson,
+  }) async {
     try {
-      final res = await _dioClient.dio.post('/$registerEndpoint', data: params);
-      return fromJson(res.data);
-    } on DioException catch (e) {
-      throw AuthErrorHandler.fromDioError(e);
-    }
-  }
-
-  // For Logout Purpose
-  Future<T> logout<T>(
-      {required String logoutEndpoint,
-      T Function(Map<String, dynamic>)? fromJson}) async {
-    try {
-      final res = await _dioClient.dio.post('/$logoutEndpoint');
-      if (fromJson != null) {
-        return fromJson(res.data);
+      Response response;
+      if (method.toUpperCase() == 'POST') {
+        response = await _dioClient.dio.post('/$endpoint', data: params);
+      } else if (method.toUpperCase() == 'GET') {
+        response =
+            await _dioClient.dio.get('/$endpoint', queryParameters: params);
+      } else {
+        throw Exception("Unsupported HTTP method: $method");
       }
-      return res.data;
+      return fromJson(response.data);
+    } on DioException catch (e) {
+      throw AuthErrorHandler.fromDioError(e);
+    }
+  }
+
+  // For Login
+  Future<T> login<T>({
+    required String loginEndpoint,
+    Map<String, dynamic>? params,
+    required T Function(Map<String, dynamic>) fromJson,
+  }) async {
+    return _request(
+      endpoint: loginEndpoint,
+      method: 'POST',
+      params: params,
+      fromJson: fromJson,
+    );
+  }
+
+  // For Register
+  Future<T> register<T>({
+    required String registerEndpoint,
+    Map<String, dynamic>? params,
+    required T Function(Map<String, dynamic>) fromJson,
+  }) async {
+    return _request(
+      endpoint: registerEndpoint,
+      method: 'POST',
+      params: params,
+      fromJson: fromJson,
+    );
+  }
+
+  // For Logout
+  Future<void> logout({required String logoutEndpoint}) async {
+    try {
+      await _dioClient.dio.post('/$logoutEndpoint');
+      await _tokenService.deleteToken();
     } on DioException catch (e) {
       throw AuthErrorHandler.fromDioError(e);
     }
