6267fbcebe6fd4b4b4a52739d82101e094777905
Netesh5 <netesh.paudel@gmail.com>
2025-03-20 15:26:31 +0545

    feat: add socialLogincubit to access login function

--- lib/src/core/cubits/social_auth_cubits/google_login_cubit.dart
+++ lib/src/core/cubits/social_auth_cubits/google_login_cubit.dart
@@ -1,13 +1,63 @@
 import 'package:flutter_authkit/flutter_authkit.dart';
+import 'package:flutter_authkit/src/core/enums/social_auth_types.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
 
-class GoogleLoginCubit extends Cubit<CommonState> {
-  GoogleLoginCubit() : super(InitialState());
+@injectable
+class SocialLoginCubit extends Cubit<CommonState> {
+  SocialLoginCubit({required this.flutterAuthKit}) : super(InitialState());
 
-  googleLogin() {
-    emit(LoadingState());
-    try {} catch (e) {
+  final FlutterAuthKit flutterAuthKit;
+
+  Future<void> socialLogin<T>({
+    required SocialAuthTypes type,
+    required String endpoint,
+    Map<String, dynamic>? params,
+    required T Function(Map<String, dynamic>) fromJson,
+  }) async {
+    try {
+      emit(LoadingState());
+
+      final res = await _handleSocialLogin<T>(
+        type: type,
+        endpoint: endpoint,
+        params: params,
+        fromJson: fromJson,
+      );
+
+      emit(SuccessState<T>(data: res));
+    } catch (e) {
       emit(ErrorState(message: e.toString()));
+    }
+  }
+
+  Future<T> _handleSocialLogin<T>({
+    required SocialAuthTypes type,
+    required String endpoint,
+    Map<String, dynamic>? params,
+    required T Function(Map<String, dynamic>) fromJson,
+  }) {
+    switch (type) {
+      case SocialAuthTypes.google:
+        return flutterAuthKit.loginWithGoogle<T>(
+          googleEndpoint: endpoint,
+          params: params,
+          fromJson: fromJson,
+        );
+      case SocialAuthTypes.facebook:
+        return flutterAuthKit.loginWithFacebook<T>(
+          facebookEndpoint: endpoint,
+          params: params,
+          fromJson: fromJson,
+        );
+      case SocialAuthTypes.apple:
+        return flutterAuthKit.loginWithApple<T>(
+          appleEndpoint: endpoint,
+          params: params,
+          fromJson: fromJson,
+        );
+      default:
+        throw UnsupportedError('Unsupported social auth type: $type');
     }
   }
 }
