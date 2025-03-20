26f34705697ecce598fc0c1ca3c653830e1f92d9
Netesh5 <netesh.paudel@gmail.com>
2025-03-20 16:01:29 +0545

    feat: clean up LogoutCubit by removing commented code and improving logOut method formatting

--- lib/src/core/cubits/email_auth_cubits/logout_cubit.dart
+++ lib/src/core/cubits/email_auth_cubits/logout_cubit.dart
@@ -5,14 +5,8 @@ import 'package:injectable/injectable.dart';
 @injectable
 class LogoutCubit extends Cubit<CommonState> {
   LogoutCubit({required this.authKit}) : super(InitialState());
-  // TEMP
-
-  // TO DO
   final FlutterAuthKit authKit;
-
-  logOut<T>({
-    String? loginEndpoint,
-  }) async {
+  logOut<T>({String? loginEndpoint}) async {
     try {
       emit(LoadingState());
       final _ = await authKit.logout();
